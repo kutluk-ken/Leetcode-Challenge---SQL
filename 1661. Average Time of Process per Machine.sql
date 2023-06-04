dd15bd88797d192cc13a9a206d953a50511c54e5
Kutluk (Ken) Anwar <56012015+kutluk-ken@users.noreply.github.com>
2023-06-04 15:46:08 -0400

    Create 1661. Average Time of Process per Machine.sql

---
+++ 1661. Average Time of Process per Machine.sql
@@ -0,0 +1,3 @@
+select a.machine_id, round(avg(end-start),3) as processing_time
+from (select machine_id, process_id,max(timestamp) as end, min(timestamp) as start from Activity group by machine_id, process_id) as a
+group by machine_id
