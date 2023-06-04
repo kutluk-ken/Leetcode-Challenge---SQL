f989e6deb297209144fde56dc6aeae887d09c5c2
Kutluk (Ken) Anwar <56012015+kutluk-ken@users.noreply.github.com>
2023-06-04 16:28:39 -0400

    Create 1193. Monthly Transactions I.sql

---
+++ 1193. Monthly Transactions I.sql
@@ -0,0 +1,5 @@
+SELECT date_format(trans_date, '%Y-%m') AS month, country, COUNT(*) AS trans_count,
+SUM(if(state = "approved",1,0)) AS approved_count, SUM(amount) AS trans_total_amount,
+SUM(if(state = "approved",amount,0)) AS approved_total_amount
+FROM Transactions
+GROUP BY month, country
